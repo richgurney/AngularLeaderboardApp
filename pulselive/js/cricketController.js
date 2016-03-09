15035b4624ed6b83123b0528df92bb316d242a02
“Richard <richgurney1987@gmail.com>
2016-03-09 23:01:35 +0000

    evening commit

---
+++ pulselive/js/cricketController.js
@@ -0,0 +1,14 @@
+angular
+  .module("cricket")
+  .controller("cricketController", cricketController);
+
+function cricketController(){
+  var self = this; 
+
+  self.closeTweet = closeTweet;
+
+  function closeTweet(){
+    alert("its working")
+  }
+}
+
